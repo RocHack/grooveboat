2891ba382cf5480590760854ad0d420b67e1bfe0
Steve Gattuso <stevenleeg@gmail.com>
2013-06-24 23:46:55 -0400

    Implements chat

--- static/application.js
+++ static/application.js
@@ -6,10 +6,17 @@ angular.module('grooveboat', [])
             .otherwise({redirect_to: "/"});
 
         $locationProvider.html5Mode(false).hashPrefix("!");
-    }]);
+    }])
+    .factory("$currentUser", function() {
+        return {
+            nickname: "",
+            clientID: ""
+        }
+    });
 
-function RoomListCtrl($scope, $location) {
+function RoomListCtrl($scope, $location, $currentUser) {
     var selected = -1;
+    $currentUser.name = "steve";
 
     $scope.rooms = [
         { name: "Ambient Electronic", selected: false },
@@ -29,11 +36,12 @@ function RoomListCtrl($scope, $location) {
     $scope.clickJoinRoom = function() {
         var room = $scope.rooms[selected];
         var name = room.name.replace(/\s/g, "-");
+        $currentUser.nickname =  $scope.nickname;
         $location.path("/room/" + name);
     }
 }
 
-function RoomCtrl($scope) {
+function RoomCtrl($scope, $currentUser) {
     $scope.djs = [
         { name: "stevenleeg", active: true },
         { name: "celehner", active: false },
@@ -55,6 +63,17 @@ function RoomCtrl($scope) {
         { from: "stevenleeg", text: "Hello there, world" },
         { from: "celehner", text: "Hello world" }
     ];
+
+    $scope.currentUser = $currentUser;
+
+    $scope.newMessage = function() {
+        $scope.chat_messages.push({
+            from: $currentUser.nickname,
+            text: $scope.message_text
+        });
+        $scope.message_text = "";
+    }
 }
 
-RoomListCtrl.$inject = ["$scope", "$location"];
+RoomListCtrl.$inject = ["$scope", "$location", "$currentUser"];
+RoomCtrl.$inject = ["$scope", "$currentUser"];
