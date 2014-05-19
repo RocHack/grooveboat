8d0ca2135f0e1abebccf0e11ecc731b7789fc2ca
Charles Lehner <celehner1@gmail.com>
2014-05-19 13:14:05 -0400

    Reconstruct blob type from data uri

--- static/database.js
+++ static/database.js
@@ -2,13 +2,14 @@
     var DB_VERSION = 2;
 
     function dataURItoBlob(dataURI) {
-        var binary = atob(dataURI.split(',')[1]);
+        var split = dataURI.split(',');
+        var binary = atob(split[1]);
         var array = [];
         for(var i = 0; i < binary.length; i++) {
             array.push(binary.charCodeAt(i));
         }
 
-        return new Blob([new Uint8Array(array)], {type: 'image/jpeg'});
+        return new Blob([new Uint8Array(array)], {type: split[0]});
     }
 
     function GrooveDB() {
