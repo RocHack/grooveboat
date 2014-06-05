c53a2ad77b9d3095924240049614ff5d9f39e2fc
Charles Lehner <celehner1@gmail.com>
2014-06-05 00:31:32 -0400

    Intercept click events for navigation router

--- browser/router.js
+++ browser/router.js
@@ -7,9 +7,12 @@ module.exports = {
 		this.observe(this.observers);
 
 		var updatePath = this.updatePath.bind(this);
-		window.addEventListener("popstate", updatePath, false);
+		var onClick = this.onClick.bind(this);
+		window.addEventListener('popstate', updatePath, false);
+		window.addEventListener('click', onClick, false);
 		this.on('teardown', function() {
-			window.removeEventListener("popstate", updatePath);
+			window.removeEventListener('popstate', updatePath, false);
+			window.removeEventListener('click', onClick, false);
 		});
 	},
 
@@ -33,7 +36,17 @@ module.exports = {
 		this.set('path', location.pathname);
 	},
 
+	onClick: function(e) {
+		if (e.target.nodeName == 'A' && !e.ctrlKey && e.target.href &&
+			e.target.href.indexOf(location.origin) === 0) {
+			// intercept the click
+			e.preventDefault();
+			this.navigate(e.target.href.substr(location.origin.length));
+		}
+	},
+
 	navigate: function(path) {
+		if (!path) path = '/';
 		this.set('path', path);
 		var page = this.get('page');
 		var state = true;
