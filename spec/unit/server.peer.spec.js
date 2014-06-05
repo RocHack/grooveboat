5c178dbf1ca54008e1a909311051034970136a7b
Sean Brennan <whalesonstilts2012@gmail.com>
2014-06-04 23:29:59 -0700

    All but one server.peer.spec.js tests are passing, need to merge in work from master

--- spec/unit/server.peer.spec.js
+++ spec/unit/server.peer.spec.js
@@ -153,74 +153,458 @@ var should = require('should')
         });
 
         describe('onSetName', function () {
-            it('should change the name of the peer');
-            it('should broadcast the name change to the room');
-            it('should do nothing when not in a room');
+            var sendAllButSpy = null;
+
+            beforeEach(function(done) {
+                sendAllButSpy = sinon.spy();
+                peer.room = { sendAllBut: sendAllButSpy };
+
+                done();
+            });
+
+            it('should change the name of the peer', function(done) {
+                peer.onSetName({ name: 'foo' });
+
+                peer.name.should.eql('foo');
+
+                done();
+            });
+
+            it('should broadcast the name change to the room', function(done) {
+                peer.id = 12345;
+                peer.onSetName({ name: 'foo' });
+
+                sendAllButSpy.calledOnce.should.be.true;
+                sendAllButSpy.calledWith(peer, 'setName', { peer: 12345, name: 'foo' });
+
+                done();
+            });
+
+            it('should do nothing when not in a room', function(done) {
+                peer.room = null;
+                peer.onSetName({ name: 'foo' });
+
+                peer.name.should.eql('foo');
+                sendAllButSpy.called.should.be.false;
+
+                done();
+            });
         });
 
         describe('onSendTo', function () {
-            it('should send a message to the other user via the buoy');
+            it('should send a message to the other user via the buoy', function(done) {
+                peer.id = 76767;
+                peer.buoy = { sendPeer: sinon.spy() };
+                peer.onSendTo({ to: 'asdf', msg: 'foo' });
+
+                peer.buoy.sendPeer.calledOnce.should.be.true;
+                peer.buoy.sendPeer.calledWith('asdf', 'recvMessage', { from: 76767, msg: 'foo' });
+
+                done();
+            });
         });
 
         describe('onJoinRoom', function () {
-            it('should set the room to the one specified');
-            it('should join the peer to the room');
+            beforeEach(function(done) {
+                peer.buoy = {
+                    getRoom: sinon.stub().returns({
+                        name: 'I like to eat yogurt',
+                        join: sinon.spy()
+                    })
+                };
+
+                done();
+            });
+
+            it('should set the room to the one specified', function(done) {
+                peer.onJoinRoom({ roomName: 'I like to eat yogurt' });
+
+                peer.buoy.getRoom.calledOnce.should.be.true;
+                peer.buoy.getRoom.calledWith('I like to eat yogurt').should.be.true;
+                peer.room.name.should.eql('I like to eat yogurt');
+
+                done();
+            });
+
+            it('should join the peer to the room', function(done) {
+                peer.onJoinRoom({ roomName: 'I like to eat yogurt' });
+
+                peer.room.join.calledOnce.should.be.true;
+                peer.room.join.calledWith(peer).should.be.true;
+
+                done();
+            });
         });
 
         describe('onLeaveRoom', function () {
-            it('should remove the peer from the room');
-            it('should clear the room property of the peer');
-            it('should do nothing when not in a room');
+            var room = null;
+
+            beforeEach(function(done) {
+                room = {
+                    leave: sinon.stub()
+                };
+
+                peer.room = room;
+
+                done();
+            });
+
+            it('should remove the peer from the room', function(done) {
+                peer.onLeaveRoom();
+
+                room.leave.calledOnce.should.be.true;
+                room.leave.calledWith(peer).should.be.true;
+
+                done();
+            });
+
+            it('should clear the room property of the peer', function(done) {
+                peer.onLeaveRoom();
+
+                (peer.room === null).should.be.true;
+
+                done();
+            });
+
+            it('should do nothing when not in a room', function(done) {
+                peer.room = null;
+                peer.onLeaveRoom();
+
+                room.leave.called.should.be.false;
+
+                done();
+            });
         });
 
         describe('onRequestDJ', function () {
-            it('should add the peer as a DJ');
-            it('should do nothing when not in a room');
+            var room = null;
+
+            beforeEach(function(done) {
+                room = {
+                    addDJ: sinon.stub()
+                };
+
+                peer.room = room;
+
+                done();
+            });
+
+            it('should add the peer as a DJ', function(done) {
+                peer.onRequestDJ();
+
+                room.addDJ.calledOnce.should.be.true;
+                room.addDJ.calledWith(peer).should.be.true;
+
+                done();
+            });
+
+            it('should do nothing when not in a room', function(done) {
+                peer.room = null;
+                peer.onRequestDJ();
+
+                room.addDJ.called.should.be.false;
+
+                done();
+            });
         });
 
         describe('onQuitDJ', function () {
-            it('should remove the peer from its DJ spot');
-            it('should do nothin when not in a room');
+            var room = null;
+
+            beforeEach(function(done) {
+                room = {
+                    removeDJ: sinon.stub()
+                };
+
+                peer.room = room;
+
+                done();
+            });
+
+            it('should remove the peer from its DJ spot', function(done) {
+                peer.onQuitDJ();
+
+                room.removeDJ.calledOnce.should.be.true;
+                room.removeDJ.calledWith(peer).should.be.true;
+
+                done();
+            });
+
+            it('should do nothin when not in a room', function(done) {
+                peer.room = null;
+                peer.onQuitDJ();
+
+                room.removeDJ.called.should.be.false;
+
+                done();
+            });
         });
 
         describe('onSetGravatar', function () {
-            it('should modify the gravatar property of the peer');
-            it('should broadcast the gravatar change when in a room');
-            it('should broadcast nothing when not in a room');
+            var room = null;
+
+            beforeEach(function(done) {
+                room = {
+                    sendAllBut: sinon.stub()
+                };
+
+                peer.room = room;
+
+                done();
+            });
+
+            it('should modify the gravatar property of the peer', function(done) {
+                peer.onSetGravatar({ gravatar: 'https://avatars1.githubusercontent.com/u/1425048?s=460' });
+
+                peer.gravatar.should.eql('https://avatars1.githubusercontent.com/u/1425048?s=460');
+
+                done();
+            });
+
+            it('should broadcast the gravatar change when in a room', function(done) {
+                peer.onSetGravatar({ gravatar: 'https://avatars1.githubusercontent.com/u/1425048?s=460' });
+
+                room.sendAllBut.calledOnce.should.be.true;
+                room.sendAllBut.calledWith(peer, 'setGravatar', {
+                    peer: peer.id,
+                    gravatar: 'https://avatars1.githubusercontent.com/u/1425048?s=460'
+                }).should.be.true;
+
+                done();
+            });
+
+            it('should broadcast nothing when not in a room', function(done) {
+                peer.room = null;
+                peer.onSetGravatar({ gravatar: 'https://avatars1.githubusercontent.com/u/1425048?s=460' });
+
+                room.sendAllBut.called.should.be.false;
+
+                done();
+            });
         });
 
         describe('onSetActiveTrack', function () {
-            it('should set the active track to the specified track');
-            it('should do nothing when not in a room');
-            it('should do nothing when not the active DJ');
-            it('should do nothing when no track is sent');
+            var track = null
+              , getActiveDJStub = null
+              , setActiveTrackStub = null;
+
+            beforeEach(function(done) {
+                track = { name: 'come on and slam', artist: 'if you wanna jam' };
+                getActiveDJStub = sinon.stub().returns(peer);
+                setActiveTrackStub = sinon.stub();
+                peer.room = {
+                    getActiveDJ: getActiveDJStub,
+                    setActiveTrack: setActiveTrackStub
+                };
+
+                done();
+            });
+
+            it('should set the active track to the specified track', function(done) {
+                peer.onSetActiveTrack({ track: track });
+
+                getActiveDJStub.calledOnce.should.be.true;
+                setActiveTrackStub.calledOnce.should.be.true;
+                setActiveTrackStub.calledWith(track).should.be.true;
+
+                done();
+            });
+
+            it('should do nothing when not in a room', function(done) {
+                peer.room = null;
+                peer.onSetActiveTrack({ track: track });
+
+                setActiveTrackStub.called.should.be.false;
+
+                done();
+            });
+
+            it('should do nothing when not the active DJ', function(done) {
+                peer.room.getActiveDJ = sinon.stub().returns(null);
+                peer.onSetActiveTrack({ track: track });
+
+                setActiveTrackStub.called.should.be.false;
+
+                done();
+            });
+
+            it('should do nothing when no track is sent', function(done) {
+                peer.onSetActiveTrack({ track: null });
+
+                setActiveTrackStub.called.should.be.false;
+
+                done();
+            });
         });
 
         describe('onSetActiveTrackDuration', function () {
-            it('should set the active track duration to the specified duration');
-            it('should do nothing when not in a room');
-            it('should do nothing when not the active DJ');
-            it('should do nothing when no duration is sent');
+            var getActiveDJStub = null
+              , setActiveTrackDurationStub = null;
+
+            beforeEach(function(done) {
+                getActiveDJStub = sinon.stub().returns(peer);
+                setActiveTrackDurationStub = sinon.stub();
+                peer.room = {
+                    getActiveDJ: getActiveDJStub,
+                    setActiveTrackDuration: setActiveTrackDurationStub
+                };
+
+                done();
+            });
+
+            it('should set the active track duration to the specified duration', function(done) {
+                peer.onSetActiveTrackDuration({ duration: 213374 });
+
+                getActiveDJStub.calledOnce.should.be.true;
+                setActiveTrackDurationStub.calledOnce.should.be.true;
+                setActiveTrackDurationStub.calledWith(213374).should.be.true;
+
+                done();
+            });
+
+            it('should do nothing when not in a room', function(done) {
+                peer.room = null;
+                peer.onSetActiveTrackDuration({ duration: 213374 });
+
+                setActiveTrackDurationStub.calledOnce.should.be.false;
+
+                done();
+            });
+
+            it('should do nothing when not the active DJ', function(done) {
+                peer.room.getActiveDJ.returns({ name: 'not you' });
+                peer.onSetActiveTrackDuration({ duration: 213374 });
+
+                setActiveTrackDurationStub.calledOnce.should.be.false;
+
+                done();
+            });
+
+            it('should do nothing when no duration is sent', function(done) {
+                peer.onSetActiveTrackDuration({ derpation: 213374 });
+
+                setActiveTrackDurationStub.calledOnce.should.be.false;
+
+                done();
+            });
         });
 
         describe('onSkip', function () {
-            it('should skip the track');
-            it('should do nothing when not in a room');
-            it('should do nothing when not the active DJ');
+            var getActiveDJStub = null
+              , skipStub = null;
+
+            beforeEach(function(done) {
+                getActiveDJStub = sinon.stub().returns(peer);
+                skipStub = sinon.stub();
+                peer.room = {
+                    getActiveDJ: getActiveDJStub,
+                    skip: skipStub
+                };
+
+                done();
+            });
+
+            it('should skip the track', function(done) {
+                peer.onSkip();
+
+                peer.room.skip.calledOnce.should.be.true;
+
+                done();
+            });
+
+            it('should do nothing when not in a room', function(done) {
+                peer.room = null;
+                peer.onSkip();
+
+                skipStub.called.should.be.false;
+
+                done();
+            });
+
+            it('should do nothing when not the active DJ', function(done) {
+                peer.room.getActiveDJ.returns({ name: 'not you' });
+                peer.onSkip();
+
+                skipStub.called.should.be.false;
+
+                done();
+            });
         });
 
         describe('onSetVote', function () {
-            it('should change the vote of the peer in the requested direction');
-            it('should broadcast the vote to all peers except the originating peer');
-            it('should do nothing if there is no track playing');
+            var room = null;
+
+            beforeEach(function(done) {
+                room = {
+                    activeTrack: { name: 'come on and slam' },
+                    sendAllBut: sinon.stub()
+                };
+
+                peer.room = room;
+
+                done();
+            });
+
+            it('should change the vote of the peer in the requested direction', function(done) {
+                peer.onSetVote({ vote: -1 });
+                peer.vote.should.eql(-1);
+
+                peer.onSetVote({ vote: -9000 });
+                peer.vote.should.eql(-1);
+
+                peer.onSetVote({ vote: 1 });
+                peer.vote.should.eql(1);
+
+                peer.onSetVote({ vote: 9000 });
+                peer.vote.should.eql(1);
+                
+                peer.onSetVote({ vote: 0 });
+                peer.vote.should.eql(0);
+
+                done();
+            });
+
+            it('should broadcast the vote to all peers except the originating peer', function(done) {
+                peer.onSetVote({ vote: 9000 });
+
+                room.sendAllBut.calledOnce.should.be.true;
+                room.sendAllBut.calledWith(peer, 'setVote', { peer: peer.id, vote: 1 });
+
+                done();
+            });
+
+            it('should do nothing if there is no track playing', function(done) {
+                peer.room.activeTrack = null;
+                peer.onSetVote({ vote: 9000 });
+
+                room.sendAllBut.called.should.be.false;
+
+                done();
+            });
         });
 
         describe('onBuoyNewRoom', function () {
-            it('should send the new room name to the client');
+            it('should send the new room name to the client', function(done) {
+                peer.send = sinon.spy();
+                peer.onBuoyNewRoom({ room: { name: 'come on and slam' } });
+
+                peer.send.calledOnce.should.be.true;
+                peer.send.calledWith('newRoom', { name: 'come on and slam' }).should.be.true;
+
+                done();
+            });
         });
 
         describe('onBuoyDeleteRoom', function () {
-            it('should send the deleted room name to the client');
+            it('should send the deleted room name to the client', function(done) {
+                peer.send = sinon.spy();
+                peer.onBuoyDeleteRoom({ room: 'I am a room for sure' });
+
+                peer.send.calledOnce.should.be.true;
+                peer.send.calledWith('deleteRoom', { name: 'I am a room for sure' }).should.be.true;
+
+                done();
+            });
         });
 
     });
